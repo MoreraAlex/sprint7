27f4f2ad5cbff840ab2464643514f9810c019925
Морера Боррото Александер Арнальдович <aamoreraborroto@gid.ru>
2026-02-03 12:31:29 +0300

    Sprint7: Убрал комментарии к заданию в классах, поправил нейминг тестового класса

--- test/ru/yandex/practicum/WordleTest.java
+++ test/ru/yandex/practicum/WordleTest.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class WordleTests {
+class WordleTest {
 
     private WordleDictionary dictionary;
     private PrintWriter log;
